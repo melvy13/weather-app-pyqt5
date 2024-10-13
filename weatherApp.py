df7a2aea26529bb1e8073f93401ef85676238d68
melvy13 <melvernlim@gmail.com>
2024-10-14 01:51:45 +0800

    gui window

--- weatherApp.py
+++ weatherApp.py
@@ -1,7 +1,17 @@
 import os
+import sys
 from dotenv import load_dotenv
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
+
+class WeatherApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
 if __name__ == "__main__":
     load_dotenv()
     api_key = os.getenv("API_KEY") # Get API_KEY from local .env file
+    app = QApplication(sys.argv)
+    weather_app = WeatherApp()
+    weather_app.show()
+    sys.exit(app.exec_())
     
